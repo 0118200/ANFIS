d68c9f2dcd4b36396e2ef148db06fab6a340a744
0118200 <donennone@gmail.com>
2025-08-08 15:27:17 +0700

    Update and rename ANFIS.m to main_control_script.m

--- main_control_script.m
+++ main_control_script.m
@@ -132,4 +132,5 @@ end
 
 %fprintf('Plot kurva kontrol ANFIS telah dibuat.\n');
 %fprintf('Rentang error: -30 hingga +30 derajat\n');
+
 %fprintf('Output kontrol: dari %.2f hingga %.2f\n', min(control_output), max(control_output));
